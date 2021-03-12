8546b24c8ed27842696a3e468d1e1ddfc134eb07
joris120 <joris.meynier@gmail.com>
2021-03-12 18:21:29 +0100

    a

--- projet/src/projet/Donjon.java
+++ projet/src/projet/Donjon.java
@@ -2,7 +2,7 @@ package projet;
 
 public class Donjon {
 	private String[][]map = new String[15][15];
-	private String[][]mapObjet = new String[15][15];
+	
 
  	public Donjon(int n) {
  		for(int z = 0; z <n; z++) {
@@ -13,21 +13,9 @@ public class Donjon {
  	 		}
  		}
  		
- 		for(int i = 0; i< 15; i++) {
- 			for(int j = 0; j < 15; j++) {
- 				mapObjet[i][j]=",";
- 			}
- 		}
- 		for(int i = 0; i< 4; i++) {
- 			for(int j = 0; j < 4; j++) {
- 				if(mapObjet[i][j].equals(",")) {
- 					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="~";
- 				}
- 				if(mapObjet[i][j].equals(",")) {
- 					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="o";
- 			}
- 			}
- 		}
+ 		
+ 		
+
  		
  	}
 
@@ -39,7 +27,7 @@ public class Donjon {
  		String r = "";
  		for (int i = 0; i < 15; i++) {
  			for(int j = 0; j<15;j++) {
- 				r += mapObjet[i][j];
+ 				r += map[i][j];
  			}
  			r+="\n";
  		}
--- projet/src/projet/Personnage.java
+++ projet/src/projet/Personnage.java
@@ -3,6 +3,35 @@ package projet;
 public class Personnage {
 	private int hp = 10;
 	private int inventaire;
+	private int i = 7;
+	private int j = 8;
+	private String[][]mapObjet = new String[15][15];
+	
+	public Personnage(int n) {
+		for(int i = 0; i< 15; i++) {
+ 			for(int j = 0; j < 15; j++) {
+ 				mapObjet[i][j]=",";
+ 			}
+ 		}
+ 		for(int i = 0; i< 4; i++) {
+ 			for(int j = 0; j < 4; j++) {
+ 				if(mapObjet[i][j].equals(",")) {
+ 					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="~";
+ 				}
+ 				if(mapObjet[i][j].equals(",")) {
+ 					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="P";
+ 				}
+ 				if(mapObjet[i][j].equals(",")) {
+ 					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="#";
+ 				}
+ 			}
+ 		}
+ 		for(int i = 0; i< 1; i++) {
+ 			for(int j = 0; j < 1; j++) {
+ 				mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="V";
+ 			}
+ 		}
+	}
 	
 	
 	public int getHp() {
@@ -21,8 +50,11 @@ public class Personnage {
 	}
 	
 	public void ramasser() {
-		if(inventaire < 5) {
+		if(inventaire < 5 && mapObjet[i][j].equals("P")) {
 			inventaire += 1;
+			if(mapObjet[i][j].equals(",")) {
+					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]="P";
+				}
 		} else {
 			System.err.println("L'inventaire est déjà plein.");
 		}
